7edc89e12f812bcc6bcd9f322057470ca341dfd7
branbon <89603405+branbon@users.noreply.github.com>
2022-12-06 21:23:39 -0500

    + updating .gitignore  + updating dependencies and socket.io

--- setup.py
+++ setup.py
@@ -5,14 +5,15 @@ setup(
     include_package_data=True,
     long_description_content_type="text/markdown",
     install_requires=[
-        "dbus-python==1.2.16",
-        "Flask==1.1.2",
-        "Flask-SocketIO==5.0.1",
-        "eventlet==0.31.0",
-        "blessed==1.17.10",
-        "pynput==1.7.1",
-        "psutil==5.6.6",
-        "cryptography==3.3.2",
+        "dbus-python==1.3.2",
+        "Flask==2.2.2",
+        "Flask-SocketIO==5.3.2",
+        "eventlet==0.33.2",
+        "blessed==1.19.1",
+        "pynput==1.7.6",
+        "psutil==5.9.4",
+        "cryptography==38.0.4",
+        "dnspython==2.2.1",
     ],
     extra_require={
         "dev": [
